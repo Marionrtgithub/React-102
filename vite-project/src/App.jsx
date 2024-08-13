e0f9de26da91def66103dfbdea4026a6bbec650f
Kiprotich marion <marionjemutai71@gmail.com>
2024-08-13 23:02:49 +0300

    Create App.jsx

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+/*import UpdateObjectstate from "./UpdateObjectstate";
+function App(){
+  return(<UpdateObjectstate />);
+}
+export default App;*/
+
+/*import UpdateArraystate from "./UpdateArraystate";
+function App(){
+  return(<UpdateArraystate/>)
+}
+export default App;*/
+/*import ArrayofobjectsUpdate from "./ArrayofobjectsUpdate";
+function App(){
+  return(<ArrayofobjectsUpdate/>);
+}
+export default App;*/
+import ToDoList from "./ToDoList";
+function App(){
+  return(<ToDoList/>)
+}
+export default App;
